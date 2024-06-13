91d34abe5e444c6f16e0f8d2208ae72aae39878b
Michael Boniface <bmg1mike@gmail.com>
2024-06-13 22:54:37 +0200

    Added file writing and reading

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount, years, expectedReturnRate float64
+
+	writeBalanceToFile();
+
+	fmt.Print("Investment Aount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	errors.New("Failed to read file")
+
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
+}
+
+func getBalanceFromFile() float64 {
+	data, err := os.ReadFile("balance.txt")
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	return balance
+}
+
+func writeBalanceToFile() {
+	balance := 1000;
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
 }
